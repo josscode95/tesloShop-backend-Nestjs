4c5300e2b611c94010bf00d594f23e8a924c927c
Jose Pardo <josmar.pm19@gmail.com>
2022-09-15 16:34:55 -0500

    add documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { AuthGuard } from "@nestjs/passport";
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { RawHeaders, GetUser, Auth } from './decorators';
 import { RoleProtected } from './decorators/role-protected.decorator';
@@ -8,6 +9,7 @@ import { User } from './entities/user.entity';
 import { UserRoleGuard } from './guards/user-role.guard';
 import { ValidRoles } from './interfaces';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,36 +1,52 @@
 import { User } from "../../auth/entities/user.entity";
 import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({ name:'products' })
 export class Product {
+
+  @ApiProperty({
+    example: 'cd533345-f1f3-48c9-a62e-7dc2da50c8f8',
+    description: 'Product ID',
+    uniqueItems: true
+  })
   @PrimaryGeneratedColumn('uuid')
   id:string;
 
+  @ApiProperty()
   @Column('text', { unique:true })
   title:string;
 
+  @ApiProperty()
   @Column('float', { default:0 })
   price:number;
 
+  @ApiProperty()
   @Column({type: 'text', nullable: true })
   description:string;
 
+  @ApiProperty()
   @Column('text', { unique:true })
   slug:string;
 
+  @ApiProperty()
   @Column('int', { default:0 })
   stock:number;
 
+  @ApiProperty()
   @Column('text', { array:true })
   sizes:string[];
 
+  @ApiProperty()
   @Column('text')
   gender:string;
 
+  @ApiProperty()
   @Column({ type:'text', array:true, default:[] })
   tags: string[];
-  
+
+  @ApiProperty()
   @OneToMany(
     () => ProductImage,
     productImage => productImage.product,
