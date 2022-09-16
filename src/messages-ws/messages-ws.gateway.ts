2157f36a00219ff99c1a823e57dff9515f89d719
Jose Pardo <josmar.pm19@gmail.com>
2022-09-15 21:52:29 -0500

    implement websockets

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,20 +1,47 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Socket, Server } from "socket.io";
+import { NewMessageDto } from './dtos/new-message.dto';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from "socket.io";
 
 @WebSocketGateway({cors:true})
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
   
+  @WebSocketServer() wss:Server;
+
   constructor(
     private readonly messagesWsService: MessagesWsService
   ) {}
 
   handleConnection(client: Socket) {
-    console.log('Cliente conectado', client.id)
+    this.messagesWsService.registerClient(client);
+    this.wss.emit('server:clients-updated', this.messagesWsService.getConnectedClients())
   }
 
   handleDisconnect(client: Socket) {
-    console.log('Cliente desconectado', client.id)
+    this.messagesWsService.removeClient(client.id);
+    this.wss.emit('server:clients-updated', this.messagesWsService.getConnectedClients())
+  }
+
+  @SubscribeMessage('front:message-oneUser')
+  handleMessageFromClient(client:Socket, payload:NewMessageDto){
+    
+    //! Emite unicamente al cliente
+    // client.emit('front:message-fromServer', {
+    //   fullName: 'Soy yo',
+    //   message: payload.message || 'no message'
+    // })
+
+    //! Emitir a todos MENOS, al cliente inicial
+    // client.broadcast.emit('front:message-fromServer', {
+    //   fullName: 'Soy yo',
+    //   message: payload.message || 'no message'
+    // })
+
+    this.wss.emit('front:message-fromServer', {
+      fullName: 'Soy yo',
+      message: payload.message || 'no message'
+    })
+
   }
 
 }
