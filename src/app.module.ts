2348a5a0380d0e1b3ffd658ff9a92f4f0c2c2110
Jose Pardo <josmar.pm19@gmail.com>
2022-09-15 19:07:56 -0500

    add websockets

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { FilesModule } from './files/files.module';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
   imports: [
@@ -30,7 +31,8 @@ import { AuthModule } from './auth/auth.module';
     CommonModule,
     SeedModule,
     FilesModule,
-    AuthModule
+    AuthModule,
+    MessagesWsModule
   ],
 })
 export class AppModule {}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,5 @@
 import { User } from "../../auth/entities/user.entity";
-import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, OptimisticLockVersionMismatchError, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./";
 import { ApiProperty } from "@nestjs/swagger";
 
@@ -14,31 +14,56 @@ export class Product {
   @PrimaryGeneratedColumn('uuid')
   id:string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'T-Shirt Teslo',
+    description: 'Product Title',
+    uniqueItems: true
+  })
   @Column('text', { unique:true })
   title:string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 0,
+    description: 'Product price'
+  })
   @Column('float', { default:0 })
   price:number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Anim reprehenderit nulla in anim mollit minim irure commodo',
+    description: 'Product description',
+    default: null
+  })
   @Column({type: 'text', nullable: true })
   description:string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 't_shirt_teslo',
+    description: 'Product SLUG - for SEO',
+    uniqueItems: true
+  })
   @Column('text', { unique:true })
   slug:string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 10,
+    description: 'Product Stock',
+    default: 0
+  })
   @Column('int', { default:0 })
   stock:number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: ['M','XL','XXL'],
+    description: 'Product Size'
+  })
   @Column('text', { array:true })
   sizes:string[];
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'women',
+    description: 'Product Gendr',
+  })
   @Column('text')
   gender:string;
 
